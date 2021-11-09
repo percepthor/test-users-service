efdc171e11f3c9d2fc62e5221b68f2c770bb0172
ermiry <erick.salas@ermiry.com>
2021-11-08 22:21:40 -0600

    Added base version methods

---
+++ service/version.py
@@ -0,0 +1,41 @@
+from cerver.utils import LOG_TYPE_NONE, cerver_log_both
+
+USERS_VERSION = "0.1"
+USERS_VERSION_NAME = "Version 0.1"
+USERS_VERSION_DATE = "21/10/2021"
+USERS_VERSION_TIME = "23:26 CST"
+USERS_VERSION_AUTHOR = "Erick Salas"
+
+def users_service_version_print_full ():
+	cerver_log_both (
+		LOG_TYPE_NONE, LOG_TYPE_NONE,
+		"\nUsers Service Version: %s".encode ("utf-8"),
+		USERS_VERSION_NAME.encode ("utf-8")
+	)
+
+	cerver_log_both (
+		LOG_TYPE_NONE, LOG_TYPE_NONE,
+		"Release Date & time: %s - %s".encode ("utf-8"),
+		USERS_VERSION_DATE.encode ("utf-8"),
+		USERS_VERSION_TIME.encode ("utf-8")
+	)
+
+	cerver_log_both (
+		LOG_TYPE_NONE, LOG_TYPE_NONE,
+		"Author: %s\n".encode ("utf-8"),
+		USERS_VERSION_AUTHOR.encode ("utf-8")
+	)
+
+def users_service_version_print_version_id ():
+	cerver_log_both (
+		LOG_TYPE_NONE, LOG_TYPE_NONE,
+		"\nUsers Service Version ID: %s\n".encode ("utf-8"),
+		USERS_VERSION.encode ("utf-8")
+	)
+
+def users_service_version_print_version_name ():
+	cerver_log_both (
+		LOG_TYPE_NONE, LOG_TYPE_NONE,
+		"\nUsers Service Version: %s\n".encode ("utf-8"),
+		USERS_VERSION_NAME.encode ("utf-8")
+	)
